06d3a60682b5039acbccc2fdeae58d7a169d2288
Kynes <kamilpirog98@gmail.com>
2025-03-13 21:52:51 +0100

    feat: handle inventory slot drag and drop

--- Assets/Scripts/UI/Inventory/InventorySlotUI.cs
+++ Assets/Scripts/UI/Inventory/InventorySlotUI.cs
@@ -1,19 +1,48 @@
 using InventorySystem;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace UI.Inventory
 {
-    public class InventorySlotUI : MonoBehaviour
+    public class InventorySlotUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
         [SerializeField] private Image itemIcon;
         [SerializeField] private TextMeshProUGUI quantityText;
+        [SerializeField] private CanvasGroup canvasGroup;
 
+        private Transform _initialParent;
+        private int _initialSiblingIndex;
+        
         public void SetItem(InventorySlot slot)
         {
             itemIcon.sprite = slot.Item.Icon;
             quantityText.text = slot.Quantity > 1 ? $"{slot.Quantity}" : "";
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            _initialParent = transform.parent;
+            _initialSiblingIndex = transform.GetSiblingIndex();
+            
+            transform.SetParent(transform.root); 
+            transform.SetAsLastSibling(); 
+            
+            canvasGroup.blocksRaycasts = false;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.position = eventData.position; 
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            transform.SetParent(_initialParent);
+            transform.SetSiblingIndex(_initialSiblingIndex);
+            
+            canvasGroup.blocksRaycasts = true;
         }
     }
 }
